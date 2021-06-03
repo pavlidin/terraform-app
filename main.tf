71b01a2ab63d67c381c8d04ff4b93f7af93f973c
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-06-04 00:59:25 +0300

    add vm resources 18

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "azurerm_virtual_network" "appnetwork" {
 
 resource "azurerm_subnet" "appsubnet" {
   count = "${var.counter}"
-  name                 = "${var.prefix}-Subnet-${count.index}"
+  name                 = "${var.prefix}-Subnet"
   resource_group_name  = azurerm_resource_group.java_app.name
   virtual_network_name = azurerm_virtual_network.appnetwork.name
   address_prefixes     = ["10.0.1.0/24"]
