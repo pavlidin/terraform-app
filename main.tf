03ee31acf71101e026b712141e432d1f025f9b78
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-06-04 01:31:14 +0300

    add vm resources 24

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ resource "random_id" "randomId" {
 }
 
 resource "azurerm_storage_account" "appstorageaccount" {
-  name                     = "storageacc${random_id.randomId.hex}"
+  name                     = "diag${random_id.randomId.hex}"
   resource_group_name      = azurerm_resource_group.java_app.name
   location                 = var.location
   account_tier             = "Standard"
