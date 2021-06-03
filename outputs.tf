fae67355afa16818fd935bca2abf8bc4c35d7896
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-06-04 02:53:07 +0300

    add vm resources 46

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "public_ip_address" {
   value = formatlist("%s", data.azurerm_public_ip.app.*.ip_address)
-  description = "The public IP address(es) of the ${var.prefix} VM(s)."
+  description = "The public IP address(es) of the VM(s)."
 }
