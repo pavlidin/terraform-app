a97c835673e5159b3a9aa71c98cd62cecad05c7c
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-06-04 02:34:16 +0300

    add vm resources 40

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "azurerm_network_security_group" "appnsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "8080"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
