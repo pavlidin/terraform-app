47c4ff85077604d6a1faf2d63bc0743aefe4e5e9
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-06-03 18:19:06 +0300

    major refactoring 7

--- main.tf
+++ main.tf
@@ -29,4 +29,4 @@ resource "azurerm_resource_group" "java_app" {
     environment = var.environment
   }
 }
-# Just a check to see if plan is automatically queued on both workspaces
+# Just a check to see if plan is automatically queued on both workspaces!
