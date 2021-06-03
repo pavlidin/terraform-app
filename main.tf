61575749c0b68827444934dd9ab35f3d624a9b27
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-06-04 02:14:30 +0300

    add vm resources 37

--- main.tf
+++ main.tf
@@ -185,7 +185,6 @@ resource "azurerm_ssh_public_key" "javaAppSSHKey" {
 }
 
 data "azurerm_public_ip" "app" {
-  count = var.counter
   name                = azurerm_public_ip.apppublicip.name
   resource_group_name = azurerm_linux_virtual_machine.appvm[count.index].resource_group_name
   depends_on          = [azurerm_linux_virtual_machine.appvm]
