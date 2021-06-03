4e145359afd7cc92531eae6e49c8f3ceca25e3fa
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-06-03 22:01:20 +0300

    add vm resources 17

--- main.tf
+++ main.tf
@@ -144,7 +144,7 @@ resource "azurerm_linux_virtual_machine" "appvm" {
   location              = var.location
   resource_group_name   = azurerm_resource_group.java_app.name
   # network_interface_ids = azurerm_network_interface.appnic[count.index]
-  network_interface_ids = ["${azurerm_network_interface.appnic[count.index]}"]
+  network_interface_ids = ["${azurerm_network_interface.appnic[count.index].id}"]
   size                  = "Standard_DS1_v2"
 
   os_disk {
